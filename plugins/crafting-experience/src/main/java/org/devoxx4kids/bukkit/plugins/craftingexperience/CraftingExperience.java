fc91687ad1859976a7267337607bf73e70338006
AdityaGupta1 <adityag@gmail.com>
2015-12-28 22:22:29 -0800

    Added the first two plugins, FirstPlugin and CraftingExperience

--- plugins/crafting-experience/src/main/java/org/devoxx4kids/bukkit/plugins/craftingexperience/CraftingExperience.java
+++ plugins/crafting-experience/src/main/java/org/devoxx4kids/bukkit/plugins/craftingexperience/CraftingExperience.java
@@ -1,14 +1,15 @@
-package org.devoxx4kids.bukkit.plugins.playerentryexit;
+package org.devoxx4kids.bukkit.plugins.craftingexperience;
 
 import java.util.logging.Level;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MyPlugin extends JavaPlugin {
+public class CraftingExperience extends JavaPlugin {
+
     // This code is called after the server starts and after the /reload command
     @Override
     public void onEnable() {
         getLogger().log(Level.INFO, "{0}.onEnable()", this.getClass().getName());
-        getServer().getPluginManager().registerEvents(new PlayerEntryExitListener(getLogger()), this);
+        getServer().getPluginManager().registerEvents(new CraftingExperienceListener(), this);
     }
 
     // This code is called before the server stops and after the /reload command
